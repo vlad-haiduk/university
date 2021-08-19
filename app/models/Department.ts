073a69125823c101cc5f6063331632e9f66c7564
vladyslav <vladgaiduk53@gmail.com>
2021-08-19 21:41:27 +0300

    init models

--- app/models/Department.ts
+++ app/models/Department.ts
@@ -1,9 +1,67 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, ObjectID, ObjectIdColumn, Column } from "typeorm";
 
 /**
  * Class Department
  */
-@Entity("departments")
+@Entity("department")
 export default class Department
 {
+
+    /**
+     * @private
+     */
+    @ObjectIdColumn()
+    private id: ObjectID;
+
+    /**
+     * @private
+     */
+    @Column()
+    private name: string;
+
+    /**
+     * @private
+     */
+    @Column()
+    private head: ObjectID;
+
+    /**
+     * @private
+     */
+    @Column()
+    private created_at: Date
+
+    /**
+     * @param name
+     * @return Department
+     */
+    public setName(name: string)
+    {
+        this.name = name;
+
+        return this;
+    }
+
+    /**
+     * @param head
+     * @return Department
+     */
+    public setHead(head: ObjectID)
+    {
+        this.head = head;
+
+        return this;
+    }
+
+    /**
+     * @param created_at
+     * @return Department
+     */
+    public setCreated(created_at: Date)
+    {
+        this.created_at = created_at;
+
+        return this;
+    }
+
 }
--- app/models/Lector.ts
+++ app/models/Lector.ts
@@ -1,9 +1,96 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, ObjectID, ObjectIdColumn, Column } from "typeorm";
 
 /**
  * Class Lector
  */
-@Entity("lectors")
+@Entity("lector")
 export default class Lector
 {
+
+    /**
+     * @private
+     */
+    @ObjectIdColumn()
+    private id: ObjectID;
+
+    /**
+     * @private
+     */
+    @Column()
+    private name: string;
+
+    /**
+     * @private
+     */
+    @Column()
+    private email: string;
+
+    /**
+     * @private
+     */
+    @Column()
+    private degree: string;
+
+    /**
+     * @private
+     */
+    @Column()
+    private salary: number
+
+    /**
+     * @private
+     */
+    @Column()
+    private departments: Array<ObjectID>
+
+    /**
+     * @private
+     */
+    @Column()
+    private created_at: Date
+
+    /**
+     * @param name
+     * @return Lector
+     */
+    public setName(name: string)
+    {
+        this.name = name;
+
+        return this;
+    }
+
+    /**
+     * @param email
+     * @return Lector
+     */
+    public setEmail(email: string)
+    {
+        this.email = email;
+
+        return this;
+    }
+
+    /**
+     * @param salary
+     * @return Lector
+     */
+    public setSalary(salary: number)
+    {
+        this.salary = salary;
+
+        return this;
+    }
+
+    /**
+     * @param created_at
+     * @return Lector
+     */
+    public setCreated(created_at: Date)
+    {
+        this.created_at = created_at;
+
+        return this;
+    }
+
 }
